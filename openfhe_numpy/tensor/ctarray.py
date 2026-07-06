1f30f2ef75f0a5c4b698152eb126cde068b1070e
Nam Bui <nam_bui@uml.edu>
2026-07-06 08:32:44 -0400

    bootstrap

--- openfhe_numpy/tensor/ctarray.py
+++ openfhe_numpy/tensor/ctarray.py
@@ -404,7 +404,7 @@ class CTArray(FHETensor[openfhe.Ciphertext]):
 
         return sum_rows_key
     
-    def bootstrap_ct(self):
+    def bootstrap(self):
         """Perform bootstrap."""
         cc = self.data.GetCryptoContext()
         self.data = cc.EvalBootstrap(self.data)
